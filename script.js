16f8627515972bf451bb68581cccbab78e89e750
Em <contactemilyhuang@gmail.com>
2021-12-14 12:23:15 -0800

    Refactor generate sketchpad function

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const sketchpadCont = document.getElementById("sketchpad");
-
 const gridClassNames = ["sixteen-dpi","thirtytwo-dpi","sixtyfour-dpi","hundred-dpi"]
 
 const cellClassNames = ["sixteen-dpi-cell","thirtytwo-dpi-cell","sixtyfour-dpi-cell","hundred-dpi-cell"]
@@ -18,21 +16,58 @@ const cellSize = {
     100: "hundred-dpi-cell"
 };
 
+const NUMBER_OF_CELLS = {
+    16: 200,
+    32: 750,
+    64: 2900,
+    100: 8000
+}
 
-function addCell(){
+const sketchpadCont = document.getElementById("sketchpad");
+
+document.body.onload = generateSketchpad(16, 200);
+
+function addCell(gridChoice){
     const newCell = document.createElement("div");
-    newCell.className = "cell sixteen-dpi-cell";
+
+    newCell.classList.add("cell",`${cellSize[gridChoice]}`)
 
     sketchpadCont.appendChild(newCell)
 }
 
-function generateSketchpad(){
-    for(i=0; i<7000; i++){
-        addCell();
+function removePreviousCells(){
+    const cell = document.querySelectorAll(".cell");
+
+    cell.forEach(function(cell){
+        cell.remove();
+    })
+}
+
+function generateSketchpad(gridChoice, NUMBER_OF_CELLS){
+    removePreviousCells();
+    for(i=0; i < NUMBER_OF_CELLS; i++){
+        addCell(gridChoice);
     }
 }
 
-document.body.onload = generateSketchpad();
+//Clicking on button will change the DPI of grid
+const buttons = document.querySelectorAll("button.grid-select");
+
+buttons.forEach(function(button){
+    button.addEventListener("click",function(){
+        let gridChoice = button.id;
+        generateSketchpad(gridChoice, NUMBER_OF_CELLS[gridChoice]);
+        changeGrid(gridChoice);
+    });
+});
+
+function changeGrid(gridChoice){
+    const currentGridClass = gridClassNames;
+
+    sketchpadCont.classList.remove(...currentGridClass);
+    sketchpadCont.classList.add(gridSize[gridChoice]);
+
+}
 
 //Bug note: if somehow clicks on 1px gap in grid, the grid lines change to black also
 sketchpadCont.addEventListener("click", function(event){
@@ -47,27 +82,3 @@ sketchpadCont.addEventListener("click", function(event){
     }
 
 })
-
-//Clicking on button will change the DPI of grid
-const buttons = document.querySelectorAll("button.grid-select");
-
-buttons.forEach(function(button){
-    button.addEventListener("click",function(){
-        let gridChoice = button.id;
-        changeGrid(gridChoice);
-    });
-});
-
-function changeGrid(gridChoice){
-    const currentGridClass = gridClassNames;
-    const currentCellClass = cellClassNames;
-    const cell = document.querySelectorAll(".cell");
-
-    sketchpadCont.classList.remove(...currentGridClass);
-    sketchpadCont.classList.add(gridSize[gridChoice]);
-
-    cell.forEach(function(cell){
-        cell.classList.remove(...currentCellClass);
-        cell.classList.add(cellSize[gridChoice]);
-    })
-}
