9d51a0793e958deb89f73291df975e6fe2c923a9
Em <contactemilyhuang@gmail.com>
2021-12-14 13:34:21 -0800

    Add drawing function

--- script.js
+++ script.js
@@ -27,10 +27,17 @@ const sketchpadCont = document.getElementById("sketchpad");
 
 document.body.onload = generateSketchpad(16, 200);
 
+//Performance note: Removing all cells and then adding the necessary amount of cells causes a bit of delay
+//Perhaps creating all necessary cells in one go and then just changing CSS classes was the best way to go for performance?
+//Alternatively consider just having to remove or add the necessary remaining amount of cells?
+
 function addCell(gridChoice){
     const newCell = document.createElement("div");
 
-    newCell.classList.add("cell",`${cellSize[gridChoice]}`)
+    //Prevent user from dragging cell when drawing
+    newCell.setAttribute("draggable", false);
+
+    newCell.classList.add("cell", cellSize[gridChoice])
 
     sketchpadCont.appendChild(newCell)
 }
@@ -70,15 +77,39 @@ function changeGrid(gridChoice){
 }
 
 //Bug note: if somehow clicks on 1px gap in grid, the grid lines change to black also
-sketchpadCont.addEventListener("click", function(event){
+//Bug note: if mouse movement is too fast, it skips pixels
+//Bug note: if mouseup happens outside of the sketch pad container, it doesn't trigger mouseup event
+//Bug note: Even though cells are set to draggable false, it is still dragging on occasion 
+// - look in to see if browser has some default drag that needs to be overridden
+
+let isDrawing = false
+
+sketchpadCont.addEventListener("mousedown", function(event){
     const gridLine = sketchpadCont
     const cell = event.target;
 
-    cell.style.backgroundColor = "black";
+    if(event.target !== gridLine){
+        cell.style.backgroundColor = "black";
+        isDrawing = true;
+    }
 
-    //Do not change grid line colour if user accidently clicks grid line
-    if(event.target === gridLine){
-        event.target.style.backgroundColor = "";
+})
+
+sketchpadCont.addEventListener("mousemove", function(event){
+    const gridLine = sketchpadCont
+    const cell = event.target;
+
+    if(isDrawing === true && event.target !== gridLine){
+        cell.style.backgroundColor = "black";
+    }
+
+})
+
+sketchpadCont.addEventListener("mouseup", function(event){
+    const gridLine = sketchpadCont
+    const cell = event.target;
+    if(isDrawing === true){
+        isDrawing = false;
     }
 
 })
