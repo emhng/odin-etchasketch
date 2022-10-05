151a695694e55422dfa1c62fa19579dd9a550474
emhng <contactemilyhuang@gmail.com>
2022-10-05 04:13:03 -0700

    Update README

--- README.md
+++ README.md
@@ -1,27 +1,32 @@
 # TOP JavaScript Basics Foundations Project - Etch-A-Sketch
-2021/12/10 ~ 2021/12/16
 
-## Objectives
+![Preview](preview.png)
+
+[Draw on Magna Doodle](https://emhng.github.io/odin-etchasketch/)
+
+## Drawing locally
+
+If you'd like to draw locally:
+
+1. Download this repo by clicking on Code -> Download zip
+2. Unzip the downloaded repo and open the folder
+3. Click on index.html
+
+## Project Objectives
 
 Using HTML, CSS, and Javascript build a browser version of something between a sketchpad and an Etch-A-Sketch.
 
 Must include:
 
-- The sketchpad grid should be made using Javascript
+- The sketchpad grid cells should be made using Javascript
 - Be able to change the resolution of the sketchpad grid (from 16x16 up to 100x100)
 - A way to clear the sketchpad
 
-Optional features:
-
-- Rainbow ink colour
-- Ink colour that starts out at 10% opacity and gets darker upon passing over it
-
-
 ## Learnings
 
-Used a lot of principles that I had used in the Rock Paper Scissors project.
+Used a lot of principles that I had used in the Rock Paper Scissors project, such as using objects as data holders.
 
-Spent a lot of time resolving bugs that involved the drawing function - 
-luckily a lot of the bugs were easily resolved by just changing what was being targeted or the order of the code
+Spent a lot of time resolving bugs that involved the drawing function -
+luckily a lot of the bugs were easily resolved by just changing what was being targeted or the order of the code.
 
 Continuing to find that checking how the site looks and functions in different browsers and screen sizes is important.
--- script.js
+++ script.js
@@ -1,130 +1,137 @@
-const gridClassNames = ["sixteen-dpi","thirtytwo-dpi","sixtyfour-dpi","hundred-dpi"]
+const gridClassNames = [
+  'sixteen-dpi',
+  'thirtytwo-dpi',
+  'sixtyfour-dpi',
+  'hundred-dpi'
+]
 
 const gridSize = {
-    16: "sixteen-dpi",
-    32: "thirtytwo-dpi",
-    64: "sixtyfour-dpi",
-    100: "hundred-dpi"
-};
-
-const cellSize = {
-    16: "sixteen-dpi-cell",
-    32: "thirtytwo-dpi-cell",
-    64: "sixtyfour-dpi-cell",
-    100: "hundred-dpi-cell"
-};
+  16: 'sixteen-dpi',
+  32: 'thirtytwo-dpi',
+  64: 'sixtyfour-dpi',
+  100: 'hundred-dpi'
+}
 
 const NUMBER_OF_CELLS = {
-    16: 230,
-    32: 880,
-    64: 3300,
-    100: 8100
+  16: 230,
+  32: 880,
+  64: 3300,
+  100: 8100
 }
 
-const sketchpadCont = document.getElementById("sketchpad");
+const sketchpadCont = document.getElementById('sketchpad')
 
-document.body.onload = generateSketchpad(16, 230);
+document.body.onload = generateSketchpad(16, 230)
 
-function generateSketchpad(gridChoice, NUMBER_OF_CELLS){
-    removePreviousCells();
-    for(i=0; i < NUMBER_OF_CELLS; i++){
-        addCell(gridChoice);
-    }
+function generateSketchpad (gridChoice, NUMBER_OF_CELLS) {
+  removePreviousCells()
+  for (i = 0; i < NUMBER_OF_CELLS; i++) {
+    addCell(gridChoice)
+  }
 }
 
-function addCell(gridChoice){
-    const newCell = document.createElement("div");
+function addCell () {
+  const newCell = document.createElement('div')
 
-    newCell.classList.add("cell", cellSize[gridChoice])
+  newCell.classList.add('cell')
 
-    sketchpadCont.appendChild(newCell)
+  sketchpadCont.appendChild(newCell)
 }
 
-function removePreviousCells(){
-    const cell = document.querySelectorAll(".cell");
+function removePreviousCells () {
+  const cell = document.querySelectorAll('.cell')
 
-    cell.forEach(function(cell){
-        cell.remove();
-    })
+  cell.forEach(function (cell) {
+    cell.remove()
+  })
 }
 
 //Clicking on grid button will change the DPI of grid
-const gridButtons = document.querySelectorAll("button.grid-select");
+const gridButtons = document.querySelectorAll('button.grid-select')
 
-gridButtons.forEach(function(button){
-    button.addEventListener("click",function(){
-        let gridChoice = button.id;
-        generateSketchpad(gridChoice, NUMBER_OF_CELLS[gridChoice]);
-        changeGrid(gridChoice);
-    });
-});
+gridButtons.forEach(function (button) {
+  button.addEventListener('click', function () {
+    let gridChoice = button.id
+    generateSketchpad(gridChoice, NUMBER_OF_CELLS[gridChoice])
+    changeGrid(gridChoice)
+  })
+})
 
-function changeGrid(gridChoice){
-    const currentGridClass = gridClassNames;
+function changeGrid (gridChoice) {
+  const currentGridClass = gridClassNames
 
-    sketchpadCont.classList.remove(...currentGridClass);
-    sketchpadCont.classList.add(gridSize[gridChoice]);
-
+  sketchpadCont.classList.remove(...currentGridClass)
+  sketchpadCont.classList.add(gridSize[gridChoice])
 }
 
 //Clicking on ink button will change the ink color
-const inkButtons = document.querySelectorAll("button.ink-select");
-let inkColor = "black";
+const inkButtons = document.querySelectorAll('button.ink-select')
+let inkColor = 'black'
 
-inkButtons.forEach(function(button){
-    button.addEventListener("click",function(){
-        let inkChoice = button.id;
-        inkColor = inkChoice;
-    });
-});
+inkButtons.forEach(function (button) {
+  button.addEventListener('click', function () {
+    let inkChoice = button.id
+    inkColor = inkChoice
+  })
+})
 
 //Continuously cycle through the rainbow in order of ROYGBIV
-const rainbow = ["red","darkorange","yellow","limegreen","blue","darkslateblue","mediumorchid"]
+const rainbow = [
+  'red',
+  'darkorange',
+  'yellow',
+  'limegreen',
+  'blue',
+  'darkslateblue',
+  'mediumorchid'
+]
 let rainbowIndex = 0
 
-function cycleRainbow(){
-    return (rainbowIndex < rainbow.length + 1) ? rainbowIndex++ : rainbowIndex = 0;
-    }
+function cycleRainbow () {
+  return rainbowIndex < rainbow.length + 1 ? rainbowIndex++ : (rainbowIndex = 0)
+}
 
 //Draw on sketchpad when mouse is pressed and held down
 //Bug note: if mouse movement is too fast, it skips pixels
 let isDrawing = false
 
-sketchpadCont.addEventListener("mousedown", function(event){
-    const gridLine = sketchpadCont
-    const cell = event.target;
+sketchpadCont.addEventListener('mousedown', function (event) {
+  const gridLine = sketchpadCont
+  const cell = event.target
 
-    isDrawing = true;
+  isDrawing = true
 
-    if(event.target !== gridLine){
-        return (inkColor === "rainbow") ? cell.style.backgroundColor = rainbow[cycleRainbow()] : cell.style.backgroundColor = inkColor;
-    }
-
+  if (event.target !== gridLine) {
+    return inkColor === 'rainbow'
+      ? (cell.style.backgroundColor = rainbow[cycleRainbow()])
+      : (cell.style.backgroundColor = inkColor)
+  }
 })
 
-sketchpadCont.addEventListener("mousemove", function(event){
-    const gridLine = sketchpadCont
-    const cell = event.target;
+sketchpadCont.addEventListener('mousemove', function (event) {
+  const gridLine = sketchpadCont
+  const cell = event.target
 
-    if(isDrawing === true && event.target !== gridLine){
-        return (inkColor === "rainbow") ? cell.style.backgroundColor = rainbow[cycleRainbow()] : cell.style.backgroundColor = inkColor;
-    }
-
+  if (isDrawing === true && event.target !== gridLine) {
+    return inkColor === 'rainbow'
+      ? (cell.style.backgroundColor = rainbow[cycleRainbow()])
+      : (cell.style.backgroundColor = inkColor)
+  }
 })
 
-document.body.addEventListener("mouseup", function(event){
-    if(isDrawing === true){
-        isDrawing = false;
-    }
+document.body.addEventListener('mouseup', function (event) {
+  if (isDrawing === true) {
+    isDrawing = false
+  }
 })
 
 //Clear sketchpad
-const eraseButton = document.getElementById("erase");
+const eraseButton = document.getElementById('erase')
 
-eraseButton.addEventListener("click",function(event){
-    const cell = document.querySelectorAll(".cell");
+eraseButton.addEventListener('click', function (event) {
+  const cell = document.querySelectorAll('.cell')
 
-    cell.forEach(function(cell){
-        cell.style.backgroundColor = null;
-    })
+  cell.forEach(function (cell) {
+    cell.style.backgroundColor = null
+  })
 })
